02501df18382a1557ed825e38cc3609fa2a1094c
megabyte22 <a.l.w.kuijper@exeter.ac.uk>
2022-07-25 20:05:40 +0100

    trying to make growth plots look decent, not easy...

--- img/growth_cp/growth_cp.py
+++ img/growth_cp/growth_cp.py
@@ -4,6 +4,72 @@ import seaborn as sns
 import pandas as pd
 import re
 
+from matplotlib import rcParams
+import matplotlib as mpl
+
+fontpath = "/System/Library/Fonts/Supplemental/" 
+fontpath = "/usr/local/texlive/2022/texmf-dist/fonts/opentype/public/stix2-otf/"
+
+mpl.use("pgf")
+
+# see http://stackoverflow.com/questions/2537868/sans-serif-math-with-latex-in-matplotlib 
+pgf_with_custom_preamble = {
+    "font.family": "serif", # use serif/main font for text elements
+    "pgf.texsystem": "xelatex",
+    "text.usetex": True,    # use inline math for ticks
+    "pgf.rcfonts": False,   # don't setup fonts from rc parameters
+    "pgf.preamble": "\n".join([
+        r"\usepackage{units}",         # load additional packages
+        r"\usepackage{mathspec}",         # load additional packages
+        r"\setmainfont[" +\
+            "Path = " + fontpath + "," +\
+            "UprightFont = *-Regular," +\
+            "ItalicFont = *-Italic," +\
+            "BoldFont = *-Bold," +\
+            "Extension = .otf]{STIXTwoText}",
+        r"\setmathfont[" +\
+            "Path = " + fontpath + "," +\
+            "UprightFont = *-Regular," +\
+            "ItalicFont = *-Italic," +\
+            "BoldFont = *-Bold," +\
+            "Extension = .otf]{STIXTwoText}",
+        r"\setmathrm[" +\
+            "Path = " + fontpath + "," +\
+            "UprightFont = *-Regular," +\
+            "ItalicFont = *-Italic," +\
+            "BoldFont = *-Bold," +\
+            "Extension = .otf]{STIXTwoText}",
+         ])
+}
+
+mpl.rcParams.update(pgf_with_custom_preamble)
+
+
+#pgf_with_custom_preamble = {
+#    "font.family": "serif", # use serif/main font for text elements
+#    "text.usetex": True,    # use inline math for ticks
+#    "pgf.rcfonts": False,   # don't setup fonts from rc parameters
+#    "pgf.texsystem": "xelatex",   # don't setup fonts from rc parameters
+#    "pgf.preamble": " ".join([
+#        r"\usepackage{units}",         # load additional packages
+#        r"\usepackage{mathspec}",         # load additional packages
+#        r"\setmainfont[" +\
+#            "UprightFont = * ," +\
+#            "ItalicFont = *Oblique," +\
+#            "BoldFont = *Bold," +\
+#            "Extension = .otf]{FreeSans}",
+#        r"\setmathsfont(Digits,Latin,Greek)[" +\
+#            "UprightFont = * ," +\
+#            "ItalicFont = *Oblique," +\
+#            "BoldFont = *Bold," +\
+#            "Extension = .otf]{FreeSans}",
+#        r"\setmathrm[" +\
+#            "UprightFont = *," +\
+#            "ItalicFont = *Oblique," +\
+#            "BoldFont = *Bold," +\
+#            "Extension = .otf]{FreeSans}",
+#         ])
+#}
 
 # function to find the start of the parameter
 # part in the data file
@@ -51,7 +117,7 @@ subset_choosy_long = pd.melt(frame=subset_choosy
         ,value_vars=["Choosy","Promiscuous"])
 
 
-the_fig = sns.relplot(
+the_ax = sns.relplot(
         data=subset_choosy_long.loc[subset_choosy_long["time"] < 20000]
         ,x="time"
         ,y="value"
@@ -59,6 +125,5 @@ the_fig = sns.relplot(
         ,kind="line"
         )
 
-the_fig
-
-the_fig.savefig(fname="plot_choosy_vs_promiscuous.pdf")
+the_ax.set(xlabel=r"Time, $t$",ylabel=r"Number infected, $I_{p\cdot}$ and $I_{c\cdot}$")
+the_ax.savefig(fname="plot_choosy_vs_promiscuous.pdf", backend="pgf")
